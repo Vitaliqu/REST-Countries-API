28253efd1f9f0f4a663ab9fc9ca6765e6c740003
vitaliip <kelezar4477@gmail.com>
2023-09-25 13:42:45 +0200

    commit

--- script/script.js
+++ script/script.js
@@ -4,23 +4,68 @@ const regionCheckbox = document.querySelectorAll(".region-checkbox");
 const themeSwitch = document.querySelector(".themeswitch")
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
 
+
 class CountryCard {
     constructor(item) {
         let {name, population, region, capital, flags} = item;
         this.name = name.common;
         this.population = new Intl.NumberFormat("en").format(population);
         this.region = region;
-        this.capital = capital
+        this.capital = capital;
         this.flag = flags.svg;
         this.card = `
-        <div class="country-card">
-            <img src=${this.flag} alt="${this.name}">
-            <p class="country-name">${this.name}</p>
-            <p class="country-population">Population: <strong>${this.population}</strong></p>
-            <p class="country-region">Region: <strong>${this.region}</strong></p>
-            <p class="country-capital">Capital: <strong>${this.capital !== undefined ? this.capital.join(", ") : "none"}</strong></p>
-        </div>`
+            <div class="country-card">
+                <img src="${this.flag}" alt="${this.name}" onclick="openCountry(this)">
+                <p class="country-name">${this.name}</p>
+                <p class="country-population">Population: <strong>${this.population}</strong></p>
+                <p class="country-region">Region: <strong>${this.region}</strong></p>
+                <p class="country-capital">Capital: <strong>${this.capital !== undefined ? this.capital.join(", ") : "none"}</strong></p>
+            </div>
+        `;
     }
+}
+
+async function openCountry(countryImage) {
+    document.body.classList.add("open-country");
+    const country = Array.from(await fetchData()).find(element => element.name.common === countryImage.parentElement.textContent.trim().split("\n")[0])
+    const scrollPosition = window.scrollY
+    const population = new Intl.NumberFormat("en").format(country.population);
+
+    const openedCountry = document.createElement("div")
+    openedCountry.className = "opened-country"
+    openedCountry.innerHTML = `
+    <div class="return-button">
+        <p>←&nbsp;Back</p>
+    </div>
+    <div class="card-container">
+        <img src="${country.flags.svg}" alt="${country.name}" class="full-size-image">
+        <div class="card-description">
+            <p class="full-size-name">${country.name.common}</p>
+            <div class="parameters-container">
+                <div class="left-parameters">
+                     Native Name: <strong>${Object.values(country.name.nativeName)[Object.values(country.name.nativeName).length - 1].common}</strong><br>
+                     Population: <strong>${population}</strong><br>
+                     Region: <strong>${country.region}</strong><br>
+                     Sub region: <strong>${country.subregion}</strong><br>
+                     Capital: <strong>${country.capital.join(", ")}</strong><br>
+                </div>
+                <div class="right-parameters">
+                     Top Level Domain: <strong>${country.tld}</strong><br>
+                     Currencies: <strong>${Object.values(country.currencies)[0].name}</strong><br>
+                     Languages: <strong>${Object.values(country.languages).join(", ")}</strong><br>
+                </div>
+            </div>
+            <div>
+            </div>
+        </div>
+    </div>`
+    box.appendChild(openedCountry);
+    console.log(Object.values(country.currencies)[0].name)
+    document.querySelector(".return-button").addEventListener("click", () => {
+        box.removeChild(openedCountry);
+        document.body.classList.remove("open-country");
+        window.scrollTo(0, scrollPosition)
+    });
 }
 
 themeSwitch.addEventListener("change", () => {
@@ -31,6 +76,7 @@ systemTheme.addListener(() => toggleTheme());
 
 searchBar.addEventListener('keyup', element => {
     fetchData().then(data => render(formater(search(data))))
+    saveData()
 })
 
 document.querySelector(".filter-checkbox").addEventListener("change", () => {
@@ -57,7 +103,6 @@ async function render(array) {
     if (array) box.innerHTML = array.join("\n");
     else fetchData().then(data => render(formater(data)))
     saveData()
-    openCountry()
 }
 
 function formater(data) {
@@ -71,8 +116,8 @@ function search(data) {
         if (checkedRegion) {
             let countryRegion = checkedRegion.parentElement.textContent
             countryRegion === "America" ? countryRegion = "Americas" : null;
-            return country.name.common.toLowerCase().includes(searchData) && country.region === countryRegion
-        } else return country.name.common.toLowerCase().includes(searchData)
+            return country.name.common.toLowerCase().startsWith(searchData) && country.region === countryRegion
+        } else return country.name.common.toLowerCase().startsWith(searchData)
     })
 }
 
@@ -84,73 +129,6 @@ function clearselection() {
     })
 }
 
-async function openCountry() {
-    for (const element of document.querySelectorAll(".country-card img")) {
-        element.addEventListener("click", async () => {
-            const country = await fetchData().then((data) => {
-                return data.filter(
-                    (country) =>
-                        country.name.common ===
-                        `${element.parentElement.textContent.split("\n")[2].trim()}`
-                )[0]
-            })
-            console.log(country)
-            const scrollPosition = window.scrollY
-            document.body.classList.add("open-country");
-
-            const returnButton = document.createElement("div");
-            returnButton.className = "return-button";
-            returnButton.innerHTML = `<p>←&nbsp;Back</p>`
-
-            const openedCountry = document.createElement("div");
-            openedCountry.className = "opened-country";
-
-            const img = document.createElement("img");
-            img.src = element.src;
-            img.className = "full-size-image"
-
-            const cardContainer = document.createElement("div");
-            cardContainer.className = "card-container"
-
-            const cardDescription = document.createElement("div")
-            cardDescription.className = "card-description"
-            const population = new Intl.NumberFormat("en").format(country.population);
-            cardDescription.innerHTML = `
-            <p class="full-size-name">${country.name.common}
-            </p>
-            <div class="parameters-container">
-                <div class="left-parameters">
-                     Native Name: <strong>${country.tld}</strong><br>
-                     Population: <strong>${population}</strong><br>
-                     Region: <strong>${country.region}</strong><br>
-                     Sub region: <strong>${country.subregion}</strong><br>
-                     Capital: <strong>${country.capital}</strong><br>
-                </div>
-                <div class="right-parameters">
-                     Top Level Domain: <strong>${country.tld}</strong><br>
-                     Currencies: <strong>${country.currencies}</strong><br>
-                     Languages: <strong>${country.languages}</strong><br>
-                
-                </div>
-            </div>
-             <div>
-             </div>`
-
-            openedCountry.appendChild(returnButton);
-            cardContainer.appendChild(img)
-            cardContainer.appendChild(cardDescription)
-            openedCountry.appendChild(cardContainer);
-
-            box.appendChild(openedCountry);
-
-            document.querySelector(".return-button").addEventListener("click", () => {
-                box.removeChild(openedCountry);
-                document.body.classList.remove("open-country");
-                window.scrollTo(0, scrollPosition)
-            });
-        });
-    }
-}
 
 function saveData() {
     let region;
@@ -163,17 +141,15 @@ function saveData() {
     localStorage.setItem('searchInput', searchBar.value)
 }
 
-if (localStorage.getItem('region') === null) {
+if (localStorage.getItem('region') === null && localStorage.getItem('searchInput') === null) {
     fetchData().then(data => render(formater(data)))
 } else {
     regionCheckbox.forEach(element => {
         if (element.parentElement.textContent === localStorage.getItem('region')) {
             element.checked = true;
-            searchBar.value = localStorage.getItem('searchInput')
-
             element.parentElement.classList.toggle("region-checked");
         }
-
+        searchBar.value = localStorage.getItem('searchInput')
     })
     fetchData().then(data => render(formater(search(data))));
 }
